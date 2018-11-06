70f0ba21eb497ee5467d7f827787caecb20119e1
u1758442 <u1758442@unimail.hud.ac.uk>
2018-11-06 16:06:46 +0000

    New lines!

--- tests/genericMethods/SwapTest.java
+++ tests/genericMethods/SwapTest.java
@@ -11,13 +11,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SwapTest {
     private Integer[] ints;
-
     private String[] phoneticAlphabet;
+    private String[] bikes;
+    private String[] films;
+    private String[][] arrays;
+
 
     @BeforeEach
     void setUp() {
         this.ints = new Integer[]{1, 2, 3, 4};
         this.phoneticAlphabet = new String[]{"Alpha", "Bravo", "Charlie", "Delta"};
+        this.bikes = new String[]{"HP4", "Panigale", "F800R", "Tiger"};
+        this.films = new String[]{"Iron man", "Fast and furious", "Mechanic", "Safe"};
+        this.arrays = new String[][]{phoneticAlphabet, bikes, films};
+
     }
 
     @AfterEach
@@ -37,14 +44,20 @@ class SwapTest {
     }
 
     @Test
-    void swapNegetive(){
+    void swapNegetiveInt(){
         assertArrayEquals(Swap.swap(this.ints, 1, 2), new Integer[]{-1, -3, -2, -4});
     }
 
     @Test
     void NegetiveIndex(){
-        assertArrayEquals(Swap.swap(this.ints, -1, -2), new Integer[]{-1, -3, -2, -4});
+        assertArrayEquals(Swap.swap(this.ints, -1, -2), new Integer[]{1, 3, 2, 4});
     }
+
+    @Test
+    void swapArrays() {
+        assertArrayEquals(Swap.swap(this.arrays, 1, 2), new String[][]{phoneticAlphabet, films, bikes});
+    }
+
 
 
 
